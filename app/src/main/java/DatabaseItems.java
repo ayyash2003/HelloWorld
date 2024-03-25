fc90f67c21f27cc5e23542ec36d40281001924a0
hp <ayyashqais99@gmail.com>
2024-03-25 03:00:48 +0200

    HelloWorld

---
+++ app/src/main/java/DatabaseItems.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class DatabaseItems {
+//    private List<MenuItem>items =new ArrayList<MenuItem>();
+//    public DatabaseItems(){
+//        items.add(new MenuItem("Coffee",10,"Hot Drink"));
+//        items.add(new MenuItem("Coffee",10,"Hot Drink"));
+//        items.add(new MenuItem("Coffee",10,"Hot Drink"));
+//
+//    }
+//    public String [] getCategories(){
+//        //Assume we are reading data from database
+//        String[] categories ={"Hot Drinks", "Cold Drinks","Sandwitch"};
+//        return categories;
+//    }
+}
---
+++ app/src/main/java/MenuItem.java
@@ -0,0 +1,14 @@
+public class MenuItem {
+    private String name ;
+    private double price ;
+    private String category ;
+    public MenuItem(){
+
+    }
+
+    public MenuItem(String name, double price, String category) {
+        this.name = name;
+        this.price = price;
+        this.category = category;
+    }
+}
